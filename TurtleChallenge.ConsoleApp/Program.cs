4fe2aaae2deddd645bf13283b1794b42f5f7f832
andreluizleite <andreluiz_leite@hotmail.com>
2025-01-21 12:41:07 +0000

    updated dotnet run parameter

--- TurtleChallenge.ConsoleApp/Program.cs
+++ TurtleChallenge.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using TurtleChallenge.Application.TurtleChallenge.Application;
+﻿using System;
+using TurtleChallenge.Application.TurtleChallenge.Application;
 using TurtleChallenge.Domain.Entities;
 using TurtleChallenge.Domain.Services;
 using TurtleChallenge.Domain.ValueObjects;
@@ -10,13 +11,28 @@ class Program
     {
         try
         {
-            var settingsFilePath = "../../../inputs/game-settings.txt";
-            var movesFilePath = "../../../inputs/moves.txt";
+            // Default file paths
+            var applicationBasePath = AppDomain.CurrentDomain.BaseDirectory;
+            var defaultInputPath = Path.Combine(applicationBasePath, "..", "..", "..", "inputs");
+            var defaultSettingsFilePath = "game-settings.txt";
+            var defaultMovesFilePath = "moves.txt";
 
+            // Use passed arguments or default files
+            var settingsFilePath = args.Length > 0 ? args[0] : defaultSettingsFilePath;
+            var movesFilePath = args.Length > 1 ? args[1] : defaultMovesFilePath;
+
+            settingsFilePath = Path.Combine(defaultInputPath, defaultSettingsFilePath);
+            movesFilePath = Path.Combine(defaultInputPath, defaultMovesFilePath);
+
+            Console.WriteLine($"Using settings file: {settingsFilePath}");
+            Console.WriteLine($"Using moves file: {movesFilePath}");
+
+            // Load game settings and moves
             var (board, _) = FileReader.LoadGameSettings(settingsFilePath);
             var moveSequences = FileReader.LoadMoves(movesFilePath);
             var gameLogic = new GameLogic(board);
 
+            // Display board information
             Console.WriteLine($"Board: {board.Width} x {board.Height}");
             Console.WriteLine($"Exit point: {board.ExitPoint.X}, {board.ExitPoint.Y}");
 
@@ -24,7 +40,7 @@ class Program
             int count = 1;
             foreach (var moveSequence in moveSequences)
             {
-                var turtle = new Turtle(new Position(0, 1), Direction.North); 
+                var turtle = new Turtle(new Position(0, 1), Direction.North);
 
                 var gameSimulator = new GameSimulator(turtle, gameLogic);
                 Console.WriteLine($"Sequence {count}: " + gameSimulator.Simulate(moveSequence));
